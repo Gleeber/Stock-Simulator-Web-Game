4feeb4b9419971cc76d92b9cb789fac69805d375
Dylan Palmieri <dgpalmieri@gmail.com>
2019-04-17 15:52:02 -0800

    Apply PEP8 conventions and refactor slightly

--- stock.py
+++ stock.py
@@ -1,17 +1,19 @@
 from flask import Blueprint, render_template
 
-class Stock():
+
+class Stock:
     def __init__(self, ticker, price, history):
         self.ticker = ticker
         self.currentPrice = price
         self.priceHistory = history
 
-    def __str__():
-        return f"${ticker}: ${price}"
+    def __str__(self):
+        return f"${self.ticker}: ${self.currentPrice}"
+
 
 stockBlueprint = Blueprint('stock', __name__, url_prefix = '/stock')
+
 
 @stockBlueprint.route('/<tickerSymbol>')
 def stockSummaryPage(tickerSymbol):
     return render_template('stockSummary.html', tickerSymbol=tickerSymbol)
-
