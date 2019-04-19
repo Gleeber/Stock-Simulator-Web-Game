e0055c99f2aaea1803a842c3982698e37f2379e6
Noah Snelson <noah.snelson@protonmail.com>
2019-04-18 20:49:54 -0800

    Added new page and route for searching stocks

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 # Main executable for stock trading web app
 
 from flask import Flask, render_template, session
-from api import apiBlueprint
+from api import apiBlueprint, searchStockData
 from stock import stockBlueprint
 from portfolio import Portfolio, serialize
-
+import json
 
 def create_app():
     app = Flask(__name__)
@@ -19,5 +19,9 @@ def create_app():
         if 'portfolio' not in session:
             session['portfolio'] = serialize(Portfolio(500, {}))
         return render_template('index.html')
-
+    @app.route('/search/<ticker>')
+    def search(ticker):
+        searchResults = searchStockData(ticker)
+        print(json.dumps(searchResults, indent=1))
+        return render_template('search.html', searchResults = searchResults)
     return app
