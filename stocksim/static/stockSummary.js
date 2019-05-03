dd4b3a29451bfa32bba761971a2b8ac865afbdf4
Noah Snelson <noah.snelson@protonmail.com>
2019-05-03 00:10:10 -0800

    Added rate limit error handling to stock summary page.

--- stocksim/static/stockSummary.js
+++ stocksim/static/stockSummary.js
@@ -4,7 +4,10 @@ function updateLatestPrice(){
     var latestPriceRequest = new XMLHttpRequest();
     var latestPriceURL = 'http://127.0.0.1:5000/api/latest/' + tickerSymbol;
     latestPriceRequest.onreadystatechange = function() {
-        document.getElementById('stockSummary').innerHTML = this.responseText;
+        if(dailyPriceHistoryRequest.readyState === 4 
+            && dailyPriceHistoryRequest.status === 200){
+            document.getElementById('stockSummary').innerHTML = this.responseText;
+        }
     }
     latestPriceRequest.open('GET', latestPriceURL, true);
     latestPriceRequest.send();
@@ -16,31 +19,35 @@ function graphPriceHistory(){
     dailyPriceHistoryRequest.responseType = 'json';
     var dailyPriceURL = 'http://127.0.0.1:5000/api/daily/' + tickerSymbol;
     dailyPriceHistoryRequest.onreadystatechange = function() {
-        if(dailyPriceHistoryRequest.readyState ==4 && dailyPriceHistoryRequest.status ==200){
-            var dailyPriceHistory = dailyPriceHistoryRequest.response;
-            var prices = [];
-            var dates = [];
-            Object.keys(dailyPriceHistory["Time Series (Daily)"]).forEach( key => {
-                var openPrice = dailyPriceHistory["Time Series (Daily)"][key]["2. high"];
-                dates.push(key);
-                prices.push(openPrice);
-            });
-            var graphElement = document.getElementById('priceHistoryGraph');
-            var config = {
-                type: 'line',
-                data: {
-                    labels: dates,
-                    datasets: [{
-                        label: "USD",
-                        data: prices,
-                        fill: true,
-                    }]
-                },
-                options: {
-                    responsive: true
-                }
-            };
-            new Chart(graphElement, config);
+        if(dailyPriceHistoryRequest.readyState === 4 
+            && dailyPriceHistoryRequest.status === 200){
+            if (dailyPriceHistoryRequest.Note === undefined){
+                var dailyPriceHistory = dailyPriceHistoryRequest.response;
+                var prices = [];
+                var dates = [];
+                Object.keys(dailyPriceHistory["Time Series (Daily)"]).forEach( key => {
+                    var openPrice = 
+                        dailyPriceHistory["Time Series (Daily)"][key]["2. high"];
+                    dates.push(key);
+                    prices.push(openPrice);
+                });
+                var graphElement = document.getElementById('priceHistoryGraph');
+                var config = {
+                    type: 'line',
+                    data: {
+                        labels: dates,
+                        datasets: [{
+                            label: "USD",
+                            data: prices,
+                            fill: true,
+                        }]
+                    },
+                    options: {
+                        responsive: true
+                    }
+                };
+                new Chart(graphElement, config);
+            }
         }
     }
     dailyPriceHistoryRequest.open('GET', dailyPriceURL, true);
